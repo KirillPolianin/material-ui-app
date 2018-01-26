17ebcb2280cbbe6fdce72004468b8021ac9c2cd2
KirillPolianin <k.polianin@gmail.com>
2018-01-27 01:01:48 +0200

    Implement form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,20 +1,105 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 import { RadioButton } from 'material-ui/RadioButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import { RadioButtonGroup, TextField } from 'redux-form-material-ui';
 
 class ContactForm extends Component {
-	render() {
-		const { handleSubmit, pristine, submitting, reset } = this.props;
-		return (
-			<form>
-				<div>
-					<Field />
-				</div>
-			</form>
-		);
-	}
+  renderPriceField() {
+    if (this.props.form.ContactForm.values.howIsPrice) {
+      return (
+        <div>
+          <Field
+            name="price"
+            component={TextField}
+            hintText="esim. Essi Merkki"
+            floatingLabelText="Oma hinta-arvio"
+          />
+        </div>
+      );
+    }
+  }
+
+  render() {
+    console.log(this.props);
+    const { handleSubmit, pristine, submitting, reset } = this.props;
+    return (
+      <form>
+        <div>
+          Annettu hinta-arvio on alustava ja saattaa poiketa oleellisesti
+          lopullisesta hinta-arviosta. Varaa maksuton asunnon tarkastus, jossa
+          saat varsinaisen hinta-arviomme ja käteistarjouksen asunnosta.
+          Tarkastus ei sido sinua vielä mihinkään. Tarkastajamme ottaa sinuun
+          yhteyttä vuorokauden sisällä.
+        </div>
+        <div>Hinta-arvio: 292 000 €</div>
+        <div>Oliko hinta arvio mielestäsi?</div>
+        <div>
+          <Field name="howIsPrice" component={RadioButtonGroup}>
+            <RadioButton value="priceIsHigh" label="Liian korkea" />
+            <RadioButton value="priceIsOk" label="Hyvä" />
+            <RadioButton value="priceIsLow" label="Liian alhainen" />
+          </Field>
+        </div>
+        {this.renderPriceField()}
+        <div>
+          <Field
+            name="name"
+            component={TextField}
+            hintText="esim. Essi Merkki"
+            floatingLabelText="Nimi"
+          />
+        </div>
+        <div>
+          <Field
+            name="address"
+            component={TextField}
+            value="gdfgdfg"
+            hintText="esim. Mannerheimintie 1 A 15 00100 Helsinki"
+            floatingLabelText="Osoite"
+          />
+        </div>
+        <div>
+          <Field
+            name="phone"
+            component={TextField}
+            hintText="esim. 040 1234567"
+            floatingLabelText="Puhelin"
+          />
+        </div>
+        <div>
+          <Field
+            name="email"
+            component={TextField}
+            hintText="esim. essi@merkki@ki.fi"
+            floatingLabelText="Sähkoposti"
+          />
+        </div>
+        <RaisedButton type="submit" label="Tilaa tarkastus" primary={true} />
+      </form>
+    );
+  }
 }
 
-export default ContactForm;
+function validate(values) {
+  const errors = {};
+  const requiredFields = ['name', 'address', 'phone', 'email'];
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = 'Required';
+    }
+  });
+
+  return errors;
+}
+
+const mapStateToProps = ({ form }) => ({ form });
+
+export default reduxForm({
+  validate,
+  form: 'ContactForm',
+  initialValues: {
+    price: 292000
+  }
+})(connect(mapStateToProps)(ContactForm));
--- src/components/ValuationForm.js
+++ src/components/ValuationForm.js
@@ -1,223 +1,249 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 import { RadioButton } from 'material-ui/RadioButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import MenuItem from 'material-ui/MenuItem';
 import {
-	RadioButtonGroup,
-	SelectField,
-	TextField
+  RadioButtonGroup,
+  SelectField,
+  Slider,
+  TextField
 } from 'redux-form-material-ui';
 
-class ValuationForm extends Component {
-	onSubmit() {
-		this.props.giveEvaluation();
-	}
+import { giveEvaluation } from '../actions';
+import ContactForm from './ContactForm';
 
-	render() {
-		const { handleSubmit, pristine, submitting, reset } = this.props;
-		return (
-			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-				<div>
-					<Field
-						key="address"
-						name="address"
-						component={TextField}
-						hintText="esim. Mannerheimintie 1"
-						floatingLabelText="Osoite"
-					/>
-				</div>
-				<div>
-					<Field
-						key="postcode"
-						name="postcode"
-						component={TextField}
-						hintText="esim. 00100"
-						floatingLabelText="Postinumero"
-					/>
-				</div>
-				<div>
-					<Field
-						key="floorNumber"
-						name="floorNumber"
-						component={SelectField}
-						floatingLabelText="Asuinkerros"
-					>
-						{[...Array(9)].map((_, index) => (
-							<MenuItem
-								key={index + 1}
-								value={index + 1}
-								primaryText={index + 1}
-							/>
-						))}
-					</Field>
-				</div>
-				<div>
-					<Field
-						key="floorCount"
-						name="floorCount"
-						component={SelectField}
-						floatingLabelText="kerroksia yhteensä"
-					>
-						{[...Array(9)].map((_, index) => (
-							<MenuItem
-								key={index + 1}
-								value={index + 1}
-								primaryText={index + 1}
-							/>
-						))}
-					</Field>
-				</div>
-				<div>
-					<Field
-						key="size"
-						name="size"
-						component={TextField}
-						hintText="esim. 45"
-						floatingLabelText="Neliöt"
-					/>
-				</div>
-				<div>
-					<Field
-						key="roomCount"
-						name="roomCount"
-						component={SelectField}
-						floatingLabelText="Huoneiden määrä"
-					>
-						{[...Array(4)].map((_, index) => (
-							<MenuItem
-								key={index + 1}
-								value={index + 1}
-								primaryText={index + 1}
-							/>
-						))}
-						<MenuItem key="5+" value="5+" primaryText="5+" />
-					</Field>
-				</div>
-				<div>
-					<Field
-						key="year"
-						name="year"
-						component={SelectField}
-						floatingLabelText="Rakennusvuosi"
-					>
-						{[...Array(219)].map((_, index) => (
-							<MenuItem
-								key={2018 - index}
-								value={2018 - index}
-								primaryText={2018 - index}
-							/>
-						))}
-					</Field>
-				</div>
-				<div>
-					<Field
-						key="cost"
-						name="cost"
-						component={TextField}
-						hintText="esim. 250"
-						floatingLabelText="Hoitovastike (€/kk)"
-					/>
-				</div>
-				<div>Tontin omistus</div>
-				<div>
-					<Field
-						key="landOwnership"
-						name="landOwnership"
-						component={RadioButtonGroup}
-					>
-						<RadioButton value="yes" label="Oma" />
-						<RadioButton value="no" label="Vuokrattu" />
-					</Field>
-				</div>
-				<div>Onko rapussa hissi</div>
-				<div>
-					<Field key="elevator" name="elevator" component={RadioButtonGroup}>
-						<RadioButton value="yes" label="Kyllä" />
-						<RadioButton value="no" label="Ei" />
-					</Field>
-				</div>
-				<div>Onko asunnossa parveke</div>
-				<div>
-					<Field key="balcony" name="balcony" component={RadioButtonGroup}>
-						<RadioButton value="yes" label="Kyllä" />
-						<RadioButton value="no" label="Ei" />
-					</Field>
-				</div>
-				<div>
-					Onko yhtiöön tulossa putkiremontti seuraavan 10 vuoden kuluessa
-				</div>
-				<div>
-					<Field
-						key="pipeRenovation"
-						name="pipeRenovation"
-						component={RadioButtonGroup}
-					>
-						<RadioButton value="yes" label="Kyllä" />
-						<RadioButton value="no" label="Ei" />
-					</Field>
-				</div>
-				<div>
-					Onko yhtiöön tulossa julkisivuremontti seuraavan 5 vuoden kuluessa
-				</div>
-				<div>
-					<Field
-						key="facadeRenovation"
-						name="facadeRenovation"
-						component={RadioButtonGroup}
-					>
-						<RadioButton value="yes" label="Kyllä" />
-						<RadioButton value="no" label="Ei" />
-					</Field>
-				</div>
-				<div>
-					<Field
-						key="condition"
-						name="condition"
-						component={SelectField}
-						floatingLabelText="Huoneiston yleiskunto"
-					>
-						<MenuItem key="5" value="5" primaryText="Erinomainen" />
-						<MenuItem key="4" value="4" primaryText="Hyvä" />
-						<MenuItem key="3" value="3" primaryText="Tyydyttävä" />
-						<MenuItem key="2" value="2" primaryText="Huono" />
-					</Field>
-				</div>
-				<RaisedButton
-					type="submit"
-					label="Tee asunnon hinta-arvio"
-					primary={true}
-				/>
-			</form>
-		);
-	}
+class ValuationForm extends Component {
+  renderContactForm() {
+    if (this.props.evaluated) {
+      return <ContactForm />;
+    }
+  }
+
+  renderButtons() {
+    const { pristine, submitting, reset } = this.props;
+    if (!this.props.evaluated) {
+      return (
+        <div>
+          <RaisedButton
+            style={{ marginRight: 12 }}
+            type="submit"
+            label="Tee asunnon hinta-arvio"
+            primary={true}
+          />
+          <RaisedButton
+            type="button"
+            disabled={pristine || submitting}
+            onClick={reset}
+            label="Clear"
+          />
+        </div>
+      );
+    }
+  }
+
+  onSubmit() {
+    this.props.giveEvaluation();
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+        <h1>Valuation Form</h1>
+        <div>
+          Toimimme toistaiseksi Helsingissä, Espoossa, Vantaalla ja Kauniaisissa
+          ja keskitymme alle 500 000€ arvoisiin kerrostalohuoneistoihin.
+        </div>
+        <div>
+          Hinta-arvio perustuu keinoälyyn ja useaan datalähteeseen, joiden
+          perusteella annamme mahdollisimman tarkan arvion. Mikäli haluat
+          lopullisen arvion asuntosi hinnasta niin suosittelemme varaamaan myös
+          maksuttoman tarkastuskäynnin, jossa tarkastajamme käy sovelluksemme
+          avulla läpi asunnon tarkemman kunnon huonekohtaisesti. Tarkastus ei
+          sido sinua vielä mihinkään
+        </div>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <div style={{ width: 500 }}>
+            <Field
+              style={{ width: 500 }}
+              name="address"
+              component={TextField}
+              hintText="esim. Mannerheimintie 1"
+              floatingLabelText="Osoite"
+            />
+          </div>
+          <div>
+            <Field
+              name="postcode"
+              component={TextField}
+              hintText="esim. 00100"
+              floatingLabelText="Postinumero"
+            />
+          </div>
+          <div>Asuinkerros</div>
+          <div>{this.props.form.ValuationForm.values.floorNumber}</div>
+          <div style={{ width: 500 }}>
+            <Field
+              name="floorNumber"
+              component={Slider}
+              format={null}
+              min={1}
+              max={9}
+              step={1}
+            />
+          </div>
+          <div>Kerroksia yhteensä</div>
+          <div>{this.props.form.ValuationForm.values.floorCount}</div>
+          <div>
+            <Field
+              name="floorCount"
+              component={Slider}
+              format={null}
+              min={1}
+              max={9}
+              step={1}
+            />
+          </div>
+          <div>
+            <Field
+              key="size"
+              name="size"
+              component={TextField}
+              hintText="esim. 45"
+              floatingLabelText="Neliöt"
+            />
+          </div>
+          <div>
+            <Field
+              key="roomCount"
+              name="roomCount"
+              component={SelectField}
+              floatingLabelText="Huoneiden määrä"
+            >
+              {[...Array(4)].map((_, index) => (
+                <MenuItem
+                  key={index + 1}
+                  value={index + 1}
+                  primaryText={index + 1}
+                />
+              ))}
+              <MenuItem key="5+" value="5+" primaryText="5+" />
+            </Field>
+          </div>
+          <div>
+            <Field
+              key="year"
+              name="year"
+              component={SelectField}
+              floatingLabelText="Rakennusvuosi"
+            >
+              {[...Array(219)].map((_, index) => (
+                <MenuItem
+                  key={2018 - index}
+                  value={2018 - index}
+                  primaryText={2018 - index}
+                />
+              ))}
+            </Field>
+          </div>
+          <div>
+            <Field
+              name="cost"
+              component={TextField}
+              hintText="esim. 250"
+              floatingLabelText="Hoitovastike (€/kk)"
+            />
+          </div>
+          <div>Tontin omistus</div>
+          <div>
+            <Field name="landOwnership" component={RadioButtonGroup}>
+              <RadioButton value="yes" label="Oma" />
+              <RadioButton value="no" label="Vuokrattu" />
+            </Field>
+          </div>
+          <div>Onko rapussa hissi</div>
+          <div>
+            <Field name="elevator" component={RadioButtonGroup}>
+              <RadioButton value="yes" label="Kyllä" />
+              <RadioButton value="no" label="Ei" />
+            </Field>
+          </div>
+          <div>Onko asunnossa parveke</div>
+          <div>
+            <Field name="balcony" component={RadioButtonGroup}>
+              <RadioButton value="yes" label="Kyllä" />
+              <RadioButton value="no" label="Ei" />
+            </Field>
+          </div>
+          <div>
+            Onko yhtiöön tulossa putkiremontti seuraavan 10 vuoden kuluessa
+          </div>
+          <div>
+            <Field name="pipeRenovation" component={RadioButtonGroup}>
+              <RadioButton value="yes" label="Kyllä" />
+              <RadioButton value="no" label="Ei" />
+            </Field>
+          </div>
+          <div>
+            Onko yhtiöön tulossa julkisivuremontti seuraavan 5 vuoden kuluessa
+          </div>
+          <div>
+            <Field name="facadeRenovation" component={RadioButtonGroup}>
+              <RadioButton value="yes" label="Kyllä" />
+              <RadioButton value="no" label="Ei" />
+            </Field>
+          </div>
+          <div>
+            <Field
+              name="condition"
+              component={SelectField}
+              floatingLabelText="Huoneiston yleiskunto"
+            >
+              <MenuItem key="5" value="5" primaryText="Erinomainen" />
+              <MenuItem key="4" value="4" primaryText="Hyvä" />
+              <MenuItem key="3" value="3" primaryText="Tyydyttävä" />
+              <MenuItem key="2" value="2" primaryText="Huono" />
+            </Field>
+          </div>
+          {this.renderButtons()}
+        </form>
+        {this.renderContactForm()}
+      </div>
+    );
+  }
 }
 
 function validate(values) {
-	const errors = {};
-	const requiredFields = ['address', 'postcode', 'size', 'cost'];
-	requiredFields.forEach(field => {
-		if (!values[field]) {
-			errors[field] = 'Required';
-		}
-	});
+  const errors = {};
+  const requiredFields = ['address', 'postcode', 'size', 'cost'];
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = 'Required';
+    }
+  });
 
-	return errors;
+  return errors;
+}
+
+function mapStateToProps({ form, evaluated }) {
+  return { form, evaluated };
 }
 
 export default reduxForm({
-	validate,
-	form: 'ValuationForm',
-	initialValues: {
-		floorNumber: 1,
-		floorCount: 1,
-		roomCount: 1,
-		year: 2018,
-		landOwnership: 'no',
-		elevator: 'no',
-		balcony: 'no',
-		pipeRenovation: 'no',
-		facadeRenovation: 'no',
-		condition: '5'
-	}
-})(ValuationForm);
+  validate,
+  form: 'ValuationForm',
+  initialValues: {
+    floorNumber: 1,
+    floorCount: 1,
+    roomCount: 1,
+    year: 2018,
+    landOwnership: 'no',
+    elevator: 'no',
+    balcony: 'no',
+    pipeRenovation: 'no',
+    facadeRenovation: 'no',
+    condition: '5'
+  }
+})(connect(mapStateToProps, { giveEvaluation })(ValuationForm));
